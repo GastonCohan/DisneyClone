cdf1ba6dfb55ffcacbb80ef6eb4fd97b44676455
gastoncohan <gaston@7r1ck.com>
2024-08-09 14:52:52 -0300

    database from firebase and functional components

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -86,7 +86,7 @@ const Wrap = styled.div`
 
   a {
     border-radius: 4px;
-    box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px --10px,
+    box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
       rgb(0 0 0 / 7) 0px 16px 10px -10px;
     cursor: pointer;
     display: block;
